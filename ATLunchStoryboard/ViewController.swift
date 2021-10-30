4ebf34f8983159fec11b79e0c0e5157f3ec448e9
Hector Castillo <h.j.castillo86@gmail.com>
2021-10-30 11:50:37 -0400

    request location permissions, add snapkit, create map view

--- ATLunchStoryboard/ViewController.swift
+++ ATLunchStoryboard/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
+import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    private var locationManager: CLLocationManager?
+
+    private let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        return mapView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+
+        view.addSubview(mapView)
+        mapView.snp.makeConstraints { make in
+            make.top.right.left.bottom.equalToSuperview()
+        }
+
     }
 
-
+    // MARK: - CLLocationManagerDelegate
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+            case .authorizedWhenInUse,.authorizedAlways:
+                print("you got it, dude")
+                print(locationManager?.location)
+            case .notDetermined:
+                manager.requestWhenInUseAuthorization()
+            case .denied, .restricted:
+                print("not gonna work, bud")
+                manager.requestWhenInUseAuthorization()
+                //graceful way to tell user they can't use the app?
+            default:
+                print("something new happened without apple telling us")
+        }
+    }
 }
 
